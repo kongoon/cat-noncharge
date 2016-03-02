9447f3303a6654121dc050f18cdb4c693ab48eb4
Manop Kongoon <kongoon@hotmail.com>
2016-03-02 16:01:00 +0700

    add instruction

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,11 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'modules' => [
+        'user' => [
+            'class' => 'dektrium\user\Module',
+        ],
+    ],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
